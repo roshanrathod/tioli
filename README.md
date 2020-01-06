3bb386245f691887f8f2f87a349d6a61b9029327
darshan1504 <djethwa2810@gmail.com>
2020-01-05 20:17:00 -0500

    fixed the ui issue for login and registration page

--- README.md
+++ README.md
@@ -5,16 +5,19 @@ Flutter Web Application
 Installation steps	
 
 Since web support is still a technical preview, you need the latest in-development version of Flutter, also referred to as the master channel. In the root folder of the Tiolo repository, run the following commands:
+
     flutter channel master
     flutter upgrade
 
 The upgrade process may take a few minutes. Next, you will need to enable web support in your Flutter installation so that it is available to this and other apps you develop on this workstation:
+
     flutter config --enable-web
     flutter devices
 
 Once web support is enabled, you will see a new Chrome device in the device list. Restart Visual Studio Code after running these commands to refresh the device list menu if you don’t see Chrome in that list yet.
 
 To add web support to the Tiolo app, you need to run this command in the top-level folder of the tioli repository:
+
     flutter create .
     flutter run -d chrome
 
--- lib/views/login_action_view.dart
+++ lib/views/login_action_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:oktoast/oktoast.dart';
 import 'package:tioli/common/global.dart';
-import 'package:tioli/widgets/centered_view/centered_view.dart';
 import 'package:tioli/widgets/inputWidget.dart';
 import 'package:tioli/services/firebase_auth.dart';
 import 'package:tioli/router.dart' as router;
@@ -9,7 +8,6 @@ import 'package:tioli/widgets/navigation/top_navigation_bar.dart';
 
 class LoginActionView extends StatefulWidget {
   final String title;
-
   LoginActionView({Key key, this.title}) : super(key: key);
   @override
   _LoginActionViewState createState() => new _LoginActionViewState();
@@ -150,179 +148,166 @@ class _LoginActionViewState extends State<LoginActionView>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: CenteredView(
-            child: Column(children: <Widget>[
-      TopNavigationBar(),
-      Expanded(
-          child: Row(children: <Widget>[
-        Padding(
-          padding: EdgeInsets.only(top: 20),
+        body: Center(
+            child: Column(
+      children: <Widget>[
+        Padding(padding: EdgeInsets.all(30),),
+        SingleChildScrollView(
+            child: ConstrainedBox(
+          constraints: BoxConstraints(
+          minHeight: MediaQuery.of(context).size.height/1.5,
+          ),
+          child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                 Container(
+                      alignment: Alignment.center,
+                      height: 90,
+                      child: TopNavigationBar()),
+                if (_isRegisterFormVisible)
+                  Container(
+                    padding: EdgeInsets.fromLTRB(10, 10, 10, 5),
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        'Nickname',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Color(0xFF999A9A),
+                        ),
+                      )),
+                if (_isRegisterFormVisible)
+                  Container(
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: <Widget>[
+                        InputWidget(
+                          0.0,
+                          0.0,
+                          false,
+                          'Pick a cool nickname for yourself ...',
+                          nicknameController,
+                        ),
+                      ],
+                    ),
+                    height: 60,
+                  ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 5),
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      'Email',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Color(0xFF999A9A),
+                      ),
+                    )),
+                Container(
+                  child: InputWidget(
+                    0.0,
+                    0.0,
+                    false,
+                    'Enter your email address to continue ...',
+                    emailController,
+                  ),
+                  height: 60,
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 5),
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      'Password',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Color(0xFF999A9A),
+                      ),
+                    )),
+                Container(
+                  child:
+                      InputWidget(
+                    0.0,
+                    0.0,
+                    true,
+                    'Enter your password to continue ...',
+                    passwordController,
+                  ),
+                  height: 60,
+                ),
+              ]),
+        )),
+        FloatingActionButton.extended(
+          onPressed: submitForm,
+          label: Text('Submit'),
+          backgroundColor: Colors.blue,
         ),
         Expanded(
-            child: Center(
-                child: Column(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(top: 10),
-            ),
-            if (_isRegisterFormVisible)
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(left: 10, bottom: 0),
-                    child: Text(
-                      'Nickname',
-                      style: TextStyle(fontSize: 16, color: Color(0xFF999A9A)),
-                    ),
-                  ),
-                  Stack(
-                    alignment: Alignment.center,
-                    children: <Widget>[
-                      InputWidget(
-                        10.0,
-                        0.0,
-                        false,
-                        'Pick a cool nickname for yourself ...',
-                        nicknameController,
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(left: 10, bottom: 0),
-                  child: Text(
-                    'Email',
-                    style: TextStyle(fontSize: 16, color: Color(0xFF999A9A)),
-                  ),
+          child: new Align(
+            alignment: Alignment.bottomCenter,
+            child: TabBar(
+              indicatorColor: Colors.blue[300],
+              labelColor: const Color(0xFF3baee7),
+              unselectedLabelColor: Colors.lightBlue[100],
+              labelStyle: TextStyle(
+                  fontFamily: 'Open Sans',
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold),
+              tabs: [
+                Tab(
+                  text: "LOGIN",
                 ),
-                Stack(
-                  alignment: Alignment.center,
-                  children: <Widget>[
-                    InputWidget(
-                        10.0,
-                        0.0,
-                        false,
-                        'Enter your email address to continue ...',
-                        emailController),
-                  ],
+                Tab(
+                  text: "REGISTER",
                 )
               ],
+              controller: _tabController,
             ),
-            Padding(
-              padding: EdgeInsets.only(top: 2),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(left: 10, bottom: 0),
-                  child: Text(
-                    'Password',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Color(0xFF999A9A),
-                    ),
-                  ),
-                ),
-                Stack(
-                  alignment: Alignment.center,
-                  children: <Widget>[
-                    InputWidget(
-                      1.0,
-                      0.0,
-                      true,
-                      'Enter your password to continue ...',
-                      passwordController,
-                    ),
-                  ],
-                )
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 20),
-            ),
-            FloatingActionButton.extended(
-              onPressed: submitForm,
-              label: Text('Submit'),
-              backgroundColor: Colors.blue,
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 30),
-            ),
-            Expanded(
-              child: new Align(
-                  alignment: Alignment.bottomCenter,
-                  child: TabBar(
-                    indicatorColor: Colors.blue[300],
-                    labelColor: const Color(0xFF3baee7),
-                    unselectedLabelColor: Colors.lightBlue[100],
-                    labelStyle: TextStyle(
-                        fontFamily: 'Open Sans',
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold),
-                    tabs: [
-                      Tab(
-                        text: "LOGIN",
-                      ),
-                      Tab(
-                        text: "REGISTER",
-                      )
-                    ],
-                    controller: _tabController,
-                  )),
-            )
-          ],
-        )))
-      ]))
-    ])));
+          ),
+        )
+      ],
+    )));
   }
-}
 
-Widget customRectButton(
-    String title, List<Color> gradient, bool isEndIconVisible) {
-  return Builder(
-    builder: (BuildContext mContext) {
-      return Padding(
-        padding: EdgeInsets.only(bottom: 10),
-        child: Stack(
-          alignment: Alignment(1.0, 0.0),
-          children: <Widget>[
-            Container(
-              alignment: Alignment.center,
-              width: MediaQuery.of(mContext).size.width / 0.5,
-              height: 50,
-              decoration: ShapeDecoration(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0)),
-                gradient: LinearGradient(
-                    colors: gradient,
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight),
+  Widget customRectButton(
+      String title, List<Color> gradient, bool isEndIconVisible) {
+    return Builder(
+      builder: (BuildContext mContext) {
+        return Padding(
+          padding: EdgeInsets.only(bottom: 10),
+          child: Stack(
+            alignment: Alignment(1.0, 0.0),
+            children: <Widget>[
+              Container(
+                alignment: Alignment.center,
+                width: MediaQuery.of(mContext).size.width / 0.5,
+                height: 50,
+                decoration: ShapeDecoration(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0)),
+                  gradient: LinearGradient(
+                      colors: gradient,
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight),
+                ),
+                child: Text(title,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500)),
               ),
-              child: Text(title,
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w500)),
-            ),
-            Visibility(
-                visible: isEndIconVisible,
-                child: Padding(
-                  padding: EdgeInsets.only(right: 10),
-                  child: ImageIcon(
-                    AssetImage("assets/logo.png"),
-                    size: 30,
-                    color: Colors.amber,
-                  ),
-                ))
-          ],
-        ),
-      );
-    },
-  );
+              Visibility(
+                  visible: isEndIconVisible,
+                  child: Padding(
+                    padding: EdgeInsets.only(right: 10),
+                    child: ImageIcon(
+                      AssetImage("assets/logo.png"),
+                      size: 30,
+                      color: Colors.amber,
+                    ),
+                  ))
+            ],
+          ),
+        );
+      },
+    );
+  }
 }
